dddfddff66943b1bab8110fe5fe9e6777dc8e91d
larayvonne <205032354+larayvonne@users.noreply.github.com>
2025-06-11 11:00:36 +0200

    cart js test

--- js/cart.js
+++ js/cart.js
@@ -1,42 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
+  const API_PATH = '../components/cart_api.php';
   document.querySelectorAll('.addToCart').forEach(btn => {
     btn.addEventListener('click', function () {
-      const id = this.dataset.id;
-      const name = this.dataset.name;
-      const description  = this.dataset.description;
-      const price = this.dataset.price;
+      btn.addEventListener('click', () => {
+      const id = btn.dataset.id;
+      const name = btn.dataset.name;
+      const description = btn.dataset.description;
+      const price = btn.dataset.price;
 
-      fetch('../components/cart_api.php', {
+      fetch(API_PATH, {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams({ action: 'add', id, description, name, price })
       })
       .then(response => {
-        if (response.ok) {
-          alert(`${name} wurde dem Warenkorb hinzugefügt.`);
-        } else {
-          alert('Fehler beim Hinzufügen zum Warenkorb.');
-        }
-      });
+          if (response.ok) {
+            alert(`${name} wurde dem Warenkorb hinzugefügt.`);
+          } else {
+            alert('Fehler beim Hinzufügen zum Warenkorb.');
+          }
+        });
     });
   });
 
   document.querySelectorAll('.removeFromCart').forEach(btn => {
-    btn.addEventListener('click', function () {
-      const id = this.dataset.id;
+     btn.addEventListener('click', () => {
+      const id = btn.dataset.id;
 
-      fetch('../components/cart_api.php', {
+
+      fetch(API_PATH, {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams({ action: 'remove', id })
       })
-      .then(response => {
-        if (response.ok) {
-          location.reload();
-        } else {
-          alert('Fehler beim Entfernen aus dem Warenkorb.');
-        }
-      });
+        .then(response => {
+          if (response.ok) {
+            location.reload();
+          } else {
+            alert('Fehler beim Entfernen aus dem Warenkorb.');
+                }
+        });
     });
   });
 });
