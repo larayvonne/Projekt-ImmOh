6b14527256b0a33d594879a47b6add8bfaa1e90f
larayvonne <205032354+larayvonne@users.noreply.github.com>
2025-06-11 13:25:50 +0200

    cart funktion

--- php/cart.php
+++ php/cart.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="de">
+<?php
+require_once "../components/dbaccess.php";
 
+?>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
