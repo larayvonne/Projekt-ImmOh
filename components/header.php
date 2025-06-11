96a7c68a61aafa41e994a7b60b6def143d378229
larayvonne <205032354+larayvonne@users.noreply.github.com>
2025-06-11 09:25:58 +0200

    nav geändert

--- components/header.php
+++ components/header.php
@@ -50,15 +50,15 @@ if (session_status() === PHP_SESSION_NONE) {
               Unternehmen
             </a>
             <ul class="dropdown-menu" aria-labelledby="bauDropdown">
-              <li><a class="dropdown-item" href="https://www.bkms-system.net/bkwebanon/report/clientInfo?cin=23WS19&c=-1&language=ger">Hinweis geben</a></li>
               <li><a class="dropdown-item" href="../php/team.php">Team</a></li>
+              <li><a class="dropdown-item" href="https://www.bkms-system.net/bkwebanon/report/clientInfo?cin=23WS19&c=-1&language=ger">Hinweis geben</a></li>
             </ul>
           </li>
 
           <!-- Dropdown Bauprojekte -->
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="bauDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Bauprojekte
+              Immobilien
             </a>
             <ul class="dropdown-menu" aria-labelledby="bauDropdown">
               <li><a class="dropdown-item" href="../php/wohnungen.php">Wohnungen</a></li>
